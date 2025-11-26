0f7455e92514f3e6dc51c86c25161497e1583766
Preetham Aacharya <149096832+Toptha@users.noreply.github.com>
2025-11-26 21:59:51 +0530

    pwa

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import Login from "./pages/Login";
 import FormPage from "./pages/Form";
 
 import "./styles.css";
+import { registerSW } from 'virtual:pwa-register'
+registerSW()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Visitor Form',
+        short_name: 'Visitors',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: []
+      }
+    })
+  ]
 })
